88b1428c79848ad0151e59093981bffee9713dac
macuguita <me@macuguita.com>
2026-07-12 20:31:51 +0200

    little fix oops

--- nix/package.nix
+++ nix/package.nix
@@ -1,6 +1,13 @@
 {
   rustPlatform,
   lib,
+  stdenv,
+  makeWrapper,
+  wayland,
+  libxkbcommon,
+  vulkan-loader,
+  libGL,
+  darwin,
 }:
 let
   fs = lib.fileset;
@@ -21,6 +28,35 @@ rustPlatform.buildRustPackage (finalAttrs: {
   cargoLock = {
     lockFile = ../Cargo.lock;
   };
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+
+  buildInputs =
+    lib.optionals stdenv.isLinux [
+      wayland
+      libxkbcommon
+      vulkan-loader
+      libGL
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.Cocoa
+      darwin.apple_sdk.frameworks.QuartzCore
+      darwin.apple_sdk.frameworks.Metal
+      darwin.apple_sdk.frameworks.AppKit
+    ];
+
+  postFixup = lib.optionalString stdenv.isLinux ''
+    wrapProgram $out/bin/${finalAttrs.pname} \
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
+        wayland
+        libxkbcommon
+        vulkan-loader
+        libGL
+      ]}
+  '';
 
   passthru = {
     cargoToml = lib.importTOML ../Cargo.toml;
